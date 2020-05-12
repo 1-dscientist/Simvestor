f013bd9df2324b056529e4e5f887105accfd3001
failstoner <jchoi182@student.fuhsd.org>
2020-05-11 21:31:25 -0700

    update stock.java
    comments and new methods to adopt with equi class

--- SimvestorApp/src/Stock.java
+++ SimvestorApp/src/Stock.java
@@ -8,90 +8,107 @@ public class Stock extends Stocks implements Equity, Transaction {
 	private boolean active;
 	
 	private static final DecimalFormat ROUND = new DecimalFormat("0.00");
-	
+	// constructor
 	public Stock(String ticker, int quantity, double buyPrice, boolean active)
-	@Override
-	public String getTicker() {
-		// TODO Auto-generated method stub
-		return null;
+	{
+		
 	}
-
 	@Override
-	public int getQuantity() {
-		// TODO Auto-generated method stub
-		return 0;
+	// gets ticker
+	public String getTicker()
+	{
+		return ticker;
 	}
-
+	
 	@Override
-	public double getBuyPrice() {
-		// TODO Auto-generated method stub
-		return 0;
+	// gets quantity
+	public int getQuantity()
+	{
+		return quantity;
 	}
-
+	//gets buy price
+	public double getBuyPrice()
+	{
+		return buyPrice;
+	}
+	// gets current price
 	@Override
-	public double getCurrentPrice() {
-		// TODO Auto-generated method stub
-		return 0;
+	public double getCurrentPrice()
+	{
+		return super.getPrice(ticker);
 	}
-
+	// gets display price
 	@Override
 	public String getDisplayPrice() {
 		// TODO Auto-generated method stub
-		return null;
+		return super.getDisplayPrice(ticker);
 	}
-
+	// gets equity value
 	@Override
 	public double getEquityValue() {
 		// TODO Auto-generated method stub
-		return 0;
+		return (double) getCurrentPrice() * quantity;
 	}
-
+	// gets total profit by subtracting buy price * quantity
 	@Override
 	public double getTotalProfit() {
 		// TODO Auto-generated method stub
-		return 0;
+		return getEquityValue()-(buyPrice*quantity);
 	}
-
+	// gets gain
 	@Override
 	public double getGain() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
+	// gets display gain
 	@Override
 	public String getDisplayGain() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	// checks transaction
 	@Override
 	public boolean isTransaction() {
 		// TODO Auto-generated method stub
 		return false;
 	}
-
+	// checks equity
 	@Override
 	public boolean isEquity() {
 		// TODO Auto-generated method stub
 		return false;
 	}
-
+	// adds to equity
 	@Override
 	public void addToEquity(int quantity, double buyPrice) {
 		// TODO Auto-generated method stub
-		
+		this.buyPrice = ((quantity*buyPrice)+(this.quantity*this.buyPrice))/(this.quantity+quantity);
+		this.quantity += quantity;
 	}
-
+	// to string
 	@Override
 	public String toStringEquity() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	// transaction to string
 	@Override
 	public String toStringTransaction() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	@Override
+	// adds 
+	public void addTo(double buyPrice, int quantity) {
+		// TODO Auto-generated method stub
+		
+	}
+	// subtracts
+	@Override
+	public void subtractFrom(double sellPrice, int quantity) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
